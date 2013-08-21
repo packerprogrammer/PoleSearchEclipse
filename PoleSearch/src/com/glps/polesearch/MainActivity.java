e3c10b8751f2298a3e6caec12f93333433c3d1d2
Garrett Hensley <hensley.garrett@gmail.com>
2013-08-21 12:17:27 -0400

    Working on version with no swipe to support older devices.

--- PoleSearch/src/com/glps/polesearch/MainActivity.java
+++ PoleSearch/src/com/glps/polesearch/MainActivity.java
@@ -35,7 +35,6 @@ import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends FragmentActivity implements
-        ActionBar.TabListener,
         LocationListener,
         GooglePlayServicesClient.OnConnectionFailedListener,
         GooglePlayServicesClient.ConnectionCallbacks {
@@ -64,16 +63,7 @@ public class MainActivity extends FragmentActivity implements
     // Handle to a SharedPreferences editor
     SharedPreferences.Editor mEditor;
 
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-     * will keep every loaded fragment in memory. If this becomes too memory
-     * intensive, it may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    SectionsPagerAdapter mSectionsPagerAdapter;
-
+   
     /**
      * The {@link ViewPager} that will host the section contents.
      */
@@ -84,42 +74,7 @@ public class MainActivity extends FragmentActivity implements
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up the action bar.
-        final ActionBar actionBar = getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the app.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                actionBar.setSelectedNavigationItem(position);
-
-
-            }
-        });
-
-        // For each of the sections in the app, add a tab to the action bar.
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
-            actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
-        }
-
+        
         hm = new Handler()
         {
             public void handleMessage(Message m) {
@@ -166,14 +121,7 @@ public class MainActivity extends FragmentActivity implements
     @Override
     public void onStop() {
 
-        // If the client is connected
-        if (mLocationClient.isConnected()) {
-            stopPeriodicUpdates();
-        }
-
-        // After disconnect() is called, the client is considered "dead".
-        mLocationClient.disconnect();
-
+       
         super.onStop();
     }
     /*
@@ -234,38 +182,6 @@ public class MainActivity extends FragmentActivity implements
         return true;
     }
     
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-        if (tab.getPosition() == 1) {
-            mUpdatesRequested = true;
-
-            if (servicesConnected()) {
-                startPeriodicUpdates();
-
-            }
-        }
-
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-
-        if (tab.getPosition() == 1){
-            mUpdatesRequested = false;
-            // If the client is connected
-            if (mLocationClient.isConnected()) {
-                stopPeriodicUpdates();
-            }
-        }
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
 
     @Override
     public void onConnected(Bundle bundle) {
@@ -335,126 +251,12 @@ public class MainActivity extends FragmentActivity implements
         }
     }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+    
+    
 
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+    
 
-        @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a DummySectionFragment (defined as a static inner class
-            // below) with the page number as its lone argument.
-            Fragment fragment; // = null;
-            if (position == 0){
-                fragment = new SearchFragment();
-            }
-            else {
-                fragment = new NearbyFragment();
-            }
-
-            //Bundle args = new Bundle();
-            //args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-            //fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 2;
-        }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
-            switch (position) {
-                case 0:
-                    return getString(R.string.title_section1).toUpperCase(l);
-                case 1:
-                    return getString(R.string.title_section2).toUpperCase(l);
-
-            }
-            return null;
-        }
-    }
-    /**
-     * A dummy fragment representing a section of the app, but that simply
-     * displays dummy text.
-     */
-    public static class DummySectionFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public DummySectionFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main_dummy, container, false);
-            TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-            dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    public static class SearchFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public SearchFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_search, container, false);
-            //TextView dummyTextView = (TextView) rootView.findViewById(R.id.section_label);
-            //dummyTextView.setText(Integer.toString(getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    public static class NearbyFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        public static final String ARG_SECTION_NUMBER = "section_number";
-
-        public NearbyFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_nearby, container, false);
-            //updateField = (EditText) rootView.findViewById(R.id.updateText);
-            latitudeField = (TextView) rootView.findViewById(R.id.TextView02);
-            longitudeField = (TextView) rootView.findViewById(R.id.TextView04);
-            textViewArray[0] = (TextView) rootView.findViewById(R.id.TextView05);
-            textViewArray[1] = (TextView) rootView.findViewById(R.id.TextView06);
-            textViewArray[2] = (TextView) rootView.findViewById(R.id.TextView07);
-            textViewArray[3] = (TextView) rootView.findViewById(R.id.TextView08);
-            textViewArray[4] = (TextView) rootView.findViewById(R.id.TextView09);
-
-
-            return rootView;
-        }
-    }
-
+   
     private boolean ValidData(EditText e){
         //Check the syntax of the data entered in the search box
 
@@ -656,30 +458,7 @@ public class MainActivity extends FragmentActivity implements
         }
     }
 
-    private boolean servicesConnected() {
-        // Check that Google Play services is available
-        int resultCode =
-                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-
-        // If Google Play services is available
-        if (ConnectionResult.SUCCESS == resultCode) {
-            // In debug mode, log the status
-            Log.d(LocationUtils.APPTAG, getString(R.string.play_services_available));
-
-            // Continue
-            return true;
-            // Google Play services was not available for some reason
-        } else {
-            // Display an error dialog
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this, 0);
-            if (dialog != null) {
-                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-                errorFragment.setDialog(dialog);
-                errorFragment.show(getFragmentManager(), LocationUtils.APPTAG);
-            }
-            return false;
-        }
-    }
+    
     private void showErrorDialog(int errorCode) {
 
         // Get the error dialog from Google Play services
@@ -705,70 +484,7 @@ public class MainActivity extends FragmentActivity implements
      * In response to a request to start updates, send a request
      * to Location Services
      */
-    private void startPeriodicUpdates() {
-
-        mLocationClient.requestLocationUpdates(mLocationRequest, this);
-
-    }
-
-    /**
-     * In response to a request to stop updates, send a request to
-     * Location Services
-     */
-    private void stopPeriodicUpdates() {
-        mLocationClient.removeLocationUpdates(this);
-
-    }
 
 
-    public void BtnStopClick (View view){
-
-        if (mLocationClient.isConnected()) {
-            stopPeriodicUpdates();
-        }
-    }
-
-
-    //Button Clicks
-    public void ShowMapClick (View view){
-        String[] sPoles = new String[5];
-        //listItems = new ArrayList<String>();
-
-        //listItems.add("012OYCOE");
-        if (listItems == null)
-        {
-            final AlertDialog.Builder NullBuilder=new AlertDialog.Builder(this);
-            NullBuilder
-                    .setTitle("No Records")
-                    .setMessage("There are no poles to display on the map.")
-                    .setCancelable(false)
-                    .setIcon(android.R.drawable.ic_dialog_alert)
-                    .setPositiveButton("OK",new DialogInterface.OnClickListener() {
-                        public void onClick(DialogInterface dialog,int id) {
-                            // if this button is clicked, close
-                            // current activity
-                            dialog.cancel();
-                        }
-                    });
-            AlertDialog NullAlert = NullBuilder.create();
-            NullAlert.show();
-            return;
-        }
-
-        for (int i = 0; i < listItems.size() && i < 5; i++){
-            sPoles[i] = (listItems.get(i));
-        }
-
-        markers = datasource.getMarkers(sPoles);
-        for (int i = 0; i < 5; i++)
-        {
-            if (markers[i] == null)
-            {
-                markers[i] = new GlpsMarker();
-                markers[i].setCoord(markers[0].getCoords()[0],markers[0].getCoords()[1]);
-            }
-        }
-        Intent intent = new Intent(getApplicationContext(), com.glps.polesearch.GLPSMapView.class);
-        startActivity(intent);
-    }
+    
 }
