02b54d8b01e8846f8a5c4942a68c446f5e8edf3e
Garrett Hensley <hensley.garrett@gmail.com>
2013-11-25 11:21:54 -0500

    don't really know

--- PoleSearch/src/com/glps/polesearch/GLPSSearchView.java
+++ PoleSearch/src/com/glps/polesearch/GLPSSearchView.java
@@ -4,14 +4,18 @@ package com.glps.polesearch;
  * Created by ghensley on 9/05/13.
  *
  */
+import java.util.List;
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -20,11 +24,14 @@ import android.view.View;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
 
 
 
-public class GLPSSearchView extends FragmentActivity {
+public class GLPSSearchView extends FragmentActivity 
+	implements OnInfoWindowClickListener {
 
 	double latValue;
 	double longValue;
@@ -58,9 +65,11 @@ public class GLPSSearchView extends FragmentActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+
         //getMenuInflater().inflate(R.menu.activity_map_view, menu);
         return true;
     }
+    
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -76,41 +85,10 @@ public class GLPSSearchView extends FragmentActivity {
 
     private void setUpMap() {
 
-
-
-        //String test = MainActivity.markers[0].getPole();
-
-        // Add lots of markers to the map.
-        addMarkersToMap();
-
-
-        // Pan to see all markers in view.
-        // Cannot zoom to bounds until the map has a size.
-        final View mapView = getSupportFragmentManager().findFragmentById(R.id.map).getView();
-        if (mapView.getViewTreeObserver().isAlive()) {
-            mapView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
-                @SuppressWarnings("deprecation") // We use the new method when supported
-                @SuppressLint("NewApi") // We check which build version we are using.
-                @Override
-                public void onGlobalLayout() {
-                   LatLngBounds bounds = new LatLngBounds.Builder()
-
-                    .include(new LatLng(latValue,longValue))
-                    //.include(new LatLng(MainActivity.markers[1].getCoords()[0],MainActivity.markers[1].getCoords()[1]))
-                   // .include(new LatLng(MainActivity.markers[2].getCoords()[0],MainActivity.markers[2].getCoords()[1]))
-                    //.include(new LatLng(MainActivity.markers[3].getCoords()[0],MainActivity.markers[3].getCoords()[1]))
-                   // .include(new LatLng(MainActivity.markers[4].getCoords()[0],MainActivity.markers[4].getCoords()[1]))
-                    .build();
-                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                        mapView.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                    } else {
-                        mapView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                    }
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, 200));
-
-                }
-            });
-        }
+    	addMarkersToMap();
+    	mMap.setOnInfoWindowClickListener(this);
+    	mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latValue,longValue), 17));
+    	
     }
     // TODO fix this
      private void addMarkersToMap() {
@@ -123,5 +101,25 @@ public class GLPSSearchView extends FragmentActivity {
                      //.snippet(MainActivity.markers[i].getPole()));
          
     }
+
+	@Override
+	public void onInfoWindowClick(Marker arg0) {
+		// TODO Auto-generated method stub
+		double lat = arg0.getPosition().latitude; 
+		double lon = arg0.getPosition().longitude;
+		String dirString = "http://maps.google.com/maps?daddr=" + lat + "," + lon;
+    	String geoloc = "http://maps.google.com/?q=test";// " + ypos + "," + xpos;
+        Intent mapIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(dirString));
+        // Verify it resolves
+        PackageManager packageManager = getPackageManager();
+        List<ResolveInfo> activities = packageManager.queryIntentActivities(mapIntent, 0);
+        boolean isIntentSafe = activities.size() > 0;
+        if (isIntentSafe) {
+            startActivity(mapIntent);
+        }//if isIntentSafe
+        
+       
+		
+	}
 }
 
